f4aabca479b8f26acc97b267e0322b38709d5a88
Lucca <lucca.serafim@gmail.com>
2024-03-20 10:07:07 -0300

    imagem

--- imagens.c
+++ imagens.c
@@ -1,20 +1,84 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-  int imagem[10][10] = {
-    {0x01010101,0x02020202,0x03030303},
-    {0x04040404,0x05050505,0x06060606},
-    {0x07070707,0x08080808,0x09090909}
-  };
-  unsigned char *p = (unsigned char *)imagem; 
-  for(int lin = 0; lin < 3*4; lin = lin+4) {
-    for(int col = 0; col < 3*4; col= col+4) {
-      printf("Pixel[%d][%d]: ", lin/4, col/4);
-      printf("A: %d,", p[lin*10 + col+0]);
-      printf("R: %d,", p[lin*10 + col+1]);
-      printf("G: %d,", p[lin*10 + col+2]);
-      printf("B: %d\n", p[lin*10 + col+3]);
+#define WIDHT 10
+#define HEIGHT 10
+
+typedef struct {
+  unsigned char r, g, b;
+
+} Pixel;
+
+void drawX(Pixel *image, int widht, int height) {
+  for(int i = 0; i < height; i++) {
+    for(int j = 0; j < widht; j++) {
+      if (i == j || i == height - 1 - j) {
+        Pixel *pixel = &image[i * widht + j];
+        pixel ->r = 0;
+        pixel ->g = 0;
+        pixel ->b = 255;
+      }
+    }
+  }
+}
+
+void drawBorder(Pixel *image, int widht, int height) {
+  for(int i = 0; i < height; i++) {
+    for(int j = 0; j < widht; j++) {
+      if(i == 0 || i == height - 1 || j == 0 || j == widht - 1) {
+        Pixel *pixel = &image[i * widht + j];
+        pixel ->r = 255;
+        pixel ->g = 0;
+        pixel ->b = 0;
+      }
+    }
+  }
+}
+
+void overlayImages(Pixel *background, Pixel *overlay, int widht, int height) {
+  for(int i = 0; i <  height; i++) {
+    for(int j = 0; j < widht; j++) {
+      Pixel *bgPixel = &background[i * widht +j];
+      Pixel *ovPixel = &overlay[i * widht + j];
+      if(ovPixel->r != 255 || ovPixel->g != 255 || ovPixel->b != 255) {
+        bgPixel->r = ovPixel->r;
+        bgPixel->g = ovPixel->g;
+        bgPixel->b = ovPixel->b;
+      }
+    }
+  }
+}
+
+void printImage(Pixel *image, int widht, int height) {
+  for(int i = 0; i < height; i++) {
+    for(int j = 0; j < widht; j++) {
+      Pixel *pixel = &image[i * widht + j];
+      printf("(%3d,%3d,%3d)", pixel->r, pixel->g, pixel->b);
     }
     printf("\n");
   }
+}
+int main() {
+
+  Pixel *image1 = (Pixel*)malloc(WIDHT * HEIGHT * sizeof(Pixel));
+  Pixel *image2 = (Pixel*)malloc(WIDHT * HEIGHT * sizeof(Pixel));
+
+  drawX(image1, WIDHT, HEIGHT);
+  printf("Imagem 1 (X):\n");
+  printImage(image1, WIDHT, HEIGHT);
+  printf("\n");
+
+  drawBorder(image2, WIDHT, HEIGHT);
+  printf("Imegem 2 (Borda):\n");
+  printImage(image2, WIDHT, HEIGHT);
+  printf("\n");
+
+  overlayImages(image1, image2, WIDHT, HEIGHT);
+  printf("Imagem final (sobreposiçao de X e borda): \n");
+  printImage(image1, WIDHT, HEIGHT);
+
+  free(image1);
+  free(image2);
+  
+
 }
